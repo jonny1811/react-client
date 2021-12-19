9db092851ea8fb39d3b9eea137a6890334706811
jonny1811 <jonny@quanto.la>
2021-12-19 19:04:02 -0300

    feat: add login with user type and show components through that

---
+++ src/pages/admin/dashboard/funcionario.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ImgFuncionario from '../../../assets/img/funcionario.png';
+
+export default function Dashboard() {
+  return (<img src={ImgFuncionario} alt="Imagen Funcionario" />);
+}
